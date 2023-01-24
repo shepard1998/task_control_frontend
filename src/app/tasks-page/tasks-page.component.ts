e716d08ad9406ec01068ae036d04732928acf6b5
Kevin <kevinfdez41@gmail.com>
2023-01-24 17:20:55 -0600

    feat: event that shows task buttons when the input is focused

--- src/app/tasks-page/tasks-page.component.ts
+++ src/app/tasks-page/tasks-page.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tasks-page',
   templateUrl: './tasks-page.component.html',
   styleUrls: ['./tasks-page.component.css']
 })
-export class TasksPageComponent {
+export class TasksPageComponent implements OnInit{
+  showDecoration!: boolean;
 
+  ngOnInit(): void { this.showDecoration = false; }
+  
+  onFocusEvent(): void
+  {
+     this.showDecoration = true;
+     const card = document.getElementById('card');
+     card?.classList.add('card-container');
+  }
+
+  onFocusOutEvent(): void
+  {
+    this.showDecoration = false;
+    const card = document.getElementById('card');
+     card?.classList.remove('card-container');
+   }
 }
